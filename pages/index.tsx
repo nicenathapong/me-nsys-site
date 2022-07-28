3ac3c60898e2577a09e00350be953617c5c4e4cf
nicenathapong <nicenathapong@gmail.com>
2022-07-28 22:04:16 +0700

    Added: Installing Next.js and Tailwind.css

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <h1 className="text-lg font-semibold">Hello World!</h1>
+        </>
+    )
+}
+
+export default Home;
